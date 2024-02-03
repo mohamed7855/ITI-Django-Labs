26e61b08a8f0713ac271c688a8acdeb72246e639
mohamed7855 <martm7726@gmail.com>
2024-02-03 15:59:46 +0200

    showing all products from DB and details for each product

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Product
 # Register your models here.
+
+# to show product table in admin page
+admin.site.register(Product)
--- product/views.py
+++ product/views.py
@@ -1,33 +1,20 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
-
-# Create your views here.
-products=[
-    {'id':1,'Name':'os','path':'os.png'},
-    {'id':2,'Name':'IOT','path':'iot.png'},
-    {'id':3,'Name':'Python','path':'python.jpeg'},
-    {'id':4,'Name':'php','path':'php.png'},
-]
+from .models import *
 
 def hello(request):
     return  render(request,'index.html')
     # return  HttpResponse('<h1>Hello Omara</h1>')
 
 def productList(request):
-    context={}
-    context['products']=products
+    context={'products':Product.objects.all}
     return  render(request,'product/index.html',context)
 
 def productdetails(request,productid):
-    product=filter(lambda t:t['id']==productid,products)
-    product=list(product)
-    context={}
-    context['product']=product
+    obj=Product.objects.get(id=productid)
+    context={'product':obj}
+    # context={'product':Product.objects.get(id=productid)}
     return  render(request,'product/details.html',context)
-    # if product:
-    #     # return HttpResponse(product)
-    #     return  render(request,'product/index.html')
-    # return HttpResponse('<span style="color:red">product not found</span>')
 
 def aboutUs(request):
     return  render(request,'aboutUs.html')
