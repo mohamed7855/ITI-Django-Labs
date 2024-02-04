e53e529fa36798d50e5a74c21e672d1da8c973fe
mohamed7855 <martm7726@gmail.com>
2024-02-04 18:06:22 +0200

    adding constraint for addProduct forms

--- product/forms.py
+++ product/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from .models import Product
+from django.core.exceptions import ValidationError
 
 class ProductForm(forms.Form):
     name = forms.CharField(min_length=3, required=True)
@@ -6,3 +8,13 @@ class ProductForm(forms.Form):
     description = forms.CharField(max_length=250)
     image = forms.ImageField()
     count = forms.IntegerField(required=True)
+
+    # add constraint for name (unique)
+    def clean_name(self):
+        userEnteredName = self.cleaned_data['name']
+        obj = Product.objects.filter(name=userEnteredName).exists()
+        if obj:
+            print(f"Product name {userEnteredName} already exist before and name is unique.")
+            raise ValidationError("name must be unique")
+        else:
+            return True
