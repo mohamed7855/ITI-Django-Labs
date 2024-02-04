a7642bf1b110463e7905bfa102b224c10831d6d0
mohamed7855 <martm7726@gmail.com>
2024-02-05 01:17:02 +0200

    adding validation to category name as unique with add category with forms

--- category/forms.py
+++ category/forms.py
@@ -8,12 +8,22 @@ class CategoryForm(forms.Form):
     image = forms.ImageField()
     age = forms.IntegerField(required=True,initial=22)
 
+    # add constraint for name (unique)
+    def clean_name(self):
+        userEnteredName = self.cleaned_data['name']
+        obj = Category.objects.filter(name=userEnteredName).exists()
+        if obj:
+            print(f"Category name {userEnteredName} already exist before and name is unique.")
+            raise ValidationError("Category name must be unique")
+        else:
+            return True
+
     # add constraint for email (unique)
     def clean_email(self):
         userEnteredEmail = self.cleaned_data['email']
         obj = Category.objects.filter(email=userEnteredEmail).exists()
         if obj:
             print(f"Category email {userEnteredEmail} already exist before and email is unique.")
-            raise ValidationError("name must be unique")
+            raise ValidationError("Category email must be unique")
         else:
             return True
