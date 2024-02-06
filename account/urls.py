6607789b54893677829836d641586a76a7632c3c
mohamed7855 <martm7726@gmail.com>
2024-02-06 15:28:16 +0200

    making authentication registration for any user

--- account/urls.py
+++ account/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('',include('django.contrib.auth.urls')),
     path('profile/',Myprofile,name="myprofile"),
+    path('register/',Registrationform.as_view(),name="Registrationform")
 ]
--- account/views.py
+++ account/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render,reverse,redirect
 from django.contrib.auth import authenticate
+from django.views.generic import CreateView
+# from  product.models import *
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 # Create your views here.
+class Registrationform(CreateView):
+    model = User
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    context_object_name = 'form'
+    success_url = reverse_lazy('login')
+
 def Myprofile(request):
     return redirect('/')
