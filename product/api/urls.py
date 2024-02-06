1aee9bbd2dff420bfbd503c87ae8d6dd499fd90d
mohamed7855 <martm7726@gmail.com>
2024-02-06 19:55:02 +0200

    showing any data that user sends

--- product/api/urls.py
+++ product/api/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('Hello/',Hello,name='hello'),
+    path('acceptData/',acceptData,name='acceptData'),
 ]
--- product/api/views.py
+++ product/api/views.py
@@ -1,7 +1,15 @@
 from rest_framework import views
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from product.models import *
 
 @api_view(['GET'])
 def Hello(request):
     return Response({'msg': 'Hello World!'})
+
+# show any data that user sends
+@api_view(['GET','POST'])
+def acceptData(request):
+    print(request.method)
+    print(request.data)
+    return Response({'msg':'Accept Data', 'data': request.data})
